acff2e714f313236c3c5e38bbb5ebe149ecd2106
MelineBYan <meline.barseghyan.91@mail.ru>
2021-05-17 00:11:12 +0400

    changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,4 @@
-export const API_HOST = process.env.REACT_APP_API_URL;
+export const API_HOST =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_URL
+    : "https://clientlist-app.herokuapp.com";
